e6e4c436d4c93409f3f53a1c810abace644c7c15
türkay ürkmez <turkay.urkmez@outlook.com>
2025-05-07 10:45:19 +0300

    introduce clean code

---
+++ Naming/Naming/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int d = 0; //Bu değişken, müşterinin kaydından itibaren geçen gün sayısını tutar.
+int daysSinceCustomerCreated = 150;
