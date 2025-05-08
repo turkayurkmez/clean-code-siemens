439f13136d037ee7fcbcee52ec3dd3a96c32f5c8
türkay ürkmez <turkay.urkmez@outlook.com>
2025-05-08 14:19:07 +0300

    Add DependencyInversion project and related classes
    - Introduced a new project "DependencyInversion" in the SOLID solution.
    - Created `DependencyInversion.csproj` with .NET 9.0 settings.
    - Implemented `ReportPublisher` class utilizing dependency injection.
    - Defined `ISender` interface with implementations: `MailSender`, `WhatsAppSender`, and `TelegramSender`.
    - Added comments for clarity on the Dependency Inversion Principle.

---
+++ SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DependencyInversion;
+
+/*
+ *  Büyük nesneler kğçğk nesnelere bağlı olmamalı. Bunun yerine bağımlı olunan nesne, dışarıdan aktarılmalı.
+ */
+
+Console.WriteLine("Hello, World!");
+
+MailSender sender = new MailSender();
+WhatsAppSender whatsSender = new WhatsAppSender();
+TelegramSender telegramSender = new TelegramSender();
+
+
+ReportPublisher reportPublisher = new ReportPublisher(telegramSender);
+//reportPublisher.Sender = sender;
+reportPublisher.Send();
+
+
+
---
+++ SOLID/DependencyInversion/ReportPublisher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public class ReportPublisher
+    {
+        //public MailSender Sender { get; set; }
+        private ISender sender;
+
+        public ReportPublisher(ISender sender)
+        {
+            this.sender = sender;
+        }
+        public void Send()
+        {
+            //MailSender sender = new MailSender();
+            sender.Send();
+        }
+    }
+
+    public interface ISender
+    {
+        void Send();
+    }
+
+    public class MailSender : ISender
+    {
+        public void Send() {
+            Console.WriteLine("Mail gönderildi");
+        }
+    }
+
+    public class WhatsAppSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Whatsapp...");
+        }
+    }
+
+    public class TelegramSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Telegram");
+        }
+    }
+}
